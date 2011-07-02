f9488851aebce4802b4634e4cd78b868aa0993c8
Adam Warski <adam@warski.org>
2011-07-02 20:48:00 +0200

    Implementing the receive method

--- core/src/main/scala/org/elasticmq/impl/NativeClientImpl.scala
+++ core/src/main/scala/org/elasticmq/impl/NativeClientImpl.scala
@@ -3,6 +3,7 @@ package org.elasticmq.impl
 import org.elasticmq.storage.Storage
 import java.util.UUID
 import org.elasticmq._
+import org.scala_tools.time.Imports._
 
 class NativeClientImpl(storage: Storage) extends Client {
   def queueClient = new NativeQueueClientImpl(storage)
@@ -32,13 +33,19 @@ class NativeMessageClientImpl(storage: Storage) extends MessageClient {
   def sendMessage(message: Message) = {
     var toSend = message
     if (toSend.id == null) toSend = toSend.copy(id = generateId())
-    if (toSend.visibilityTimeout == null) toSend = toSend.copy(visibilityTimeout = message.queue.defaultVisibilityTimeout)
+    if (toSend.visibilityTimeout == 0) toSend = toSend.copy(visibilityTimeout = message.queue.defaultVisibilityTimeout)
     storage.messageStorage.persistMessage(toSend)
     toSend
   }
 
-  def receiveMessage(queue: Queue) = {
-    null
+  def receiveMessage(queue: Queue): Option[Message] = {
+    val now = DateTime.now.millis
+
+    storage.messageStorage.lookupPendingMessage(queue, now)
+      .flatMap(message =>
+        storage.messageStorage
+                .updateLastDelivered(message, now)
+                .orElse(receiveMessage(queue)))
   }
 
   def updateVisibilityTimeout(message: Message, newVisibilityTimeout: Long) = {
