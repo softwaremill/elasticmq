f1e07acd1a0d1749e1daa765aca39b5621aebf91
Adam Warski <adam@warski.org>
2011-07-21 14:52:58 +0200

    Rest path internal DSL

---
+++ rest/rest-core/src/main/scala/org/elasticmq/rest/RestPath.scala
@@ -0,0 +1,100 @@
+package org.elasticmq.rest
+
+import util.matching.Regex
+
+abstract class RestPath {
+  val next: Option[RestPath]
+  def matchAndExtract(path: String): Option[Map[String, String]]
+
+  protected def nextMatchAndExtract(rest: Option[String]): Option[Map[String, String]] = {
+    (next, rest) match {
+      case (Some(rp), Some(path)) => rp.matchAndExtract(path)
+      case (None, None) => Some(Map())
+      case _ => None
+    }
+  }
+
+  def nextComponent(path: String) = {
+    val splitResult = path.split("/", 2)
+    val component = splitResult(0)
+    val rest = if (splitResult.length > 1) Some(splitResult(1)) else None
+
+    (component, rest)
+  }
+
+  def copy(newNext: Option[RestPath]): RestPath
+
+  def singleToString: String
+  override def toString = "/"+singleToString+(next match { case Some(rp) => rp.toString case None => "" })
+}
+
+case class StringRestPath(part: String, next: Option[RestPath]) extends RestPath {
+  def matchAndExtract(path: String) = {
+    val (component, rest) = nextComponent(path)
+    if (component == part) nextMatchAndExtract(rest) else None
+  }
+
+  def copy(newNext: Option[RestPath]) = StringRestPath(part, newNext)
+
+  override def singleToString = part
+}
+
+case class RegexRestPath(regex: Regex, next: Option[RestPath]) extends RestPath {
+  def matchAndExtract(path: String) = {
+    val (component, rest) = nextComponent(path)
+    if (regex.pattern.matcher(component).matches) nextMatchAndExtract(rest) else None
+  }
+
+  def copy(newNext: Option[RestPath]) = RegexRestPath(regex, newNext)
+
+  override def singleToString = regex.toString()
+}
+
+case class ParamRestPath(param: RestPathParam, next: Option[RestPath]) extends RestPath {
+  def matchAndExtract(path: String) = {
+    val (component, rest) = nextComponent(path)
+    nextMatchAndExtract(rest).map(_ + (param.name -> component))
+  }
+
+  def copy(newNext: Option[RestPath]) = ParamRestPath(param, newNext)
+
+  override def singleToString = "%"+param.name
+}
+
+class RestPathCreator(val previous: List[RestPath]) {
+  def /(part: String) = new RestPathCreator(new StringRestPath(part, None) :: previous)
+  def /(regex: Regex) = new RestPathCreator(new RegexRestPath(regex, None) :: previous)
+  def /(param: RestPathParam) = new RestPathCreator(new ParamRestPath(param, None) :: previous)
+
+  def build(pathList: List[RestPath], next: Option[RestPath]): Option[RestPath] = {
+    pathList match {
+      case Nil => next
+      case path :: pathListTail => build(pathListTail, Some(path.copy(next)))
+    }
+  }
+
+  def build(): RestPath = build(previous, None) match {
+    // Special case: root path
+    case None => StringRestPath("", None)
+    case Some(rp) => rp
+  }
+}
+
+case class RestPathParam(name: String)
+
+/**
+ * Usage example:
+ * <pre>
+ * import RestPath._
+ * root / "a" / "b" / "c"
+ * root / "a" / """[a-zA-Z]+[0-9]""".r / "c"
+ * root / "a" / $("p1") / "c"
+ * </pre>
+ */
+object RestPath {
+  def root = new RestPathCreator(Nil)
+
+  def %(name: String) = RestPathParam(name)
+
+  implicit def restPathCreatorToRestPath(rpc: RestPathCreator): RestPath = rpc.build()
+}
---
+++ rest/rest-core/src/test/scala/org/elasticmq/rest/RestPathTestSuite.scala
@@ -0,0 +1,28 @@
+package org.elasticmq.rest
+
+import org.scalatest.FunSuite
+import org.scalatest.matchers.MustMatchers
+import org.scalatest.prop.TableDrivenPropertyChecks
+
+class RestPathTestSuite extends FunSuite with MustMatchers with TableDrivenPropertyChecks {
+  import RestPath._
+
+  val paths = Table[RestPath, String, Option[Map[String, String]]](
+    ("rest path", "input path", "expected result"),
+    (root / "a" / "b", "a/b", Some(Map())),
+    (root / "a" / "b", "a/c", None),
+    (root / "a" / "b", "a", None),
+    (root / "a" / "b", "a/b/d", None),
+    (root / "a" / """[a-z][0-9]+""".r, "a/aa", None),
+    (root / "a" / """[a-z][0-9]+""".r, "a/a123", Some(Map())),
+    (root / "a" / %("p1") / "c", "a/zzz/c", Some(Map("p1" -> "zzz"))),
+    (root / "a" / %("p1") / %("p2"), "a/zzz/yyy", Some(Map("p1" -> "zzz", "p2" -> "yyy"))),
+    (root / "a" / %("p1") / %("p2"), "a/zzz", None)
+  )
+
+  forAll (paths) { (restPath, inputPath, expectedResult) =>
+    test(restPath+" for "+inputPath+" should give result "+expectedResult) {
+      restPath.matchAndExtract(inputPath) must equal (expectedResult)
+    }
+  }
+}
