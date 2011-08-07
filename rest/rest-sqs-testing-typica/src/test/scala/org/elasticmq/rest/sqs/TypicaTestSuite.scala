7bec2e3dfa31aaac17b49bc9e009b6e129c2d217
Adam Warski <adam@warski.org>
2011-08-07 15:48:51 +0200

    Very simple test for creating a queue + queue creating logic

--- rest/rest-sqs-testing-typica/src/test/scala/org/elasticmq/rest/sqs/TypicaTestSuite.scala
+++ rest/rest-sqs-testing-typica/src/test/scala/org/elasticmq/rest/sqs/TypicaTestSuite.scala
@@ -5,10 +5,15 @@ import org.scalatest.{BeforeAndAfter, FunSuite}
 import org.elasticmq.rest.RestServer
 import org.elasticmq.{Node, NodeBuilder}
 import com.xerox.amazonws.sqs2.{QueueService, SQSUtils}
+import org.apache.log4j.BasicConfigurator
+import org.jboss.netty.logging.{Log4JLoggerFactory, InternalLoggerFactory}
 
 class TypicaTestSuite extends FunSuite with MustMatchers with BeforeAndAfter {
   var node: Node = _
   var server: RestServer = _
+
+  BasicConfigurator.configure();
+  InternalLoggerFactory.setDefaultFactory(new Log4JLoggerFactory())
 
   before {
     node = NodeBuilder.createNode
@@ -21,7 +26,7 @@ class TypicaTestSuite extends FunSuite with MustMatchers with BeforeAndAfter {
   }
 
   test("should create a queue") {
-    //val queueService = new QueueService("n/a", "n/a", false, "localhost", 8888)
-    //queueService.getOrCreateMessageQueue("testQueue1")
+    val queueService = new QueueService("n/a", "n/a", false, "localhost", 8888)
+    queueService.getOrCreateMessageQueue("testQueue1")
   }
 }
--- rest/rest-sqs/src/main/scala/org/elasticmq/rest/sqs/SQSRestServerFactory.scala
+++ rest/rest-sqs/src/main/scala/org/elasticmq/rest/sqs/SQSRestServerFactory.scala
@@ -6,19 +6,57 @@ import org.jboss.netty.handler.codec.http.{HttpRequest, HttpMethod}
 import org.elasticmq.rest.{StringResponse, RequestHandlerLogic, RestServer}
 import org.elasticmq.{MillisVisibilityTimeout, Queue, Client}
 
+import HttpMethod._
+
 class SQSRestServerFactory(client: Client) {
-  val createQueueHandler = (createHandler
-            forMethod HttpMethod.GET
-            forPath (root / "test" / "me")
-            requiringQueryParameters List("param1")
-            running (new RequestHandlerLogic() {
-      def handle(request: HttpRequest, parameters: Map[String, String]) = {
-        client.queueClient.createQueue(Queue("test", MillisVisibilityTimeout(1000L)))
-        StringResponse("OK", "text/plain")
-      }
-    }))
+  val queueNameParameter = "QueueName"
+  val actionParameter = "Action"
+
+  val createQueueAction = actionParameter -> "CreateQueue"
+
+  val createQueueLogic = new RequestHandlerLogic() {
+    def handle(request: HttpRequest, parameters: Map[String, String]) = {
+      client.queueClient.createQueue(Queue(parameters(queueNameParameter), MillisVisibilityTimeout(1000L)))
+
+      val response =
+        <CreateQueueResponse
+          xmlns="http://queue.amazonaws.com/doc/2009-02-01/"
+          xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+          xsi:type="CreateQueueResponse">
+          <CreateQueueResult>
+            <QueueUrl>
+              http://sqs.us-east-1.amazonaws.com/123456789012/testQueue
+            </QueueUrl>
+          </CreateQueueResult>
+          <ResponseMetadata>
+            <RequestId>
+              7a62c49f-347e-4fc4-9331-6e8e7a96aa73
+            </RequestId>
+          </ResponseMetadata>
+        </CreateQueueResponse>
+
+      StringResponse(response.toString(), "text/plain")
+    }
+  }
+
+  val createQueueGetHandler = (createHandler
+            forMethod GET
+            forPath (root)
+            requiringParameters List(queueNameParameter)
+            requiringParameterValues Map(createQueueAction)
+            running createQueueLogic)
+
+  val createQueuePostHandler = (createHandler
+            forMethod POST
+            forPath (root)
+            includingParametersFromBody ()
+            requiringParameters List(queueNameParameter)
+            requiringParameterValues Map(createQueueAction)
+            running createQueueLogic)
 
   def start(port: Int): RestServer = {
-    RestServer.start(createQueueHandler :: Nil, port)
+    RestServer.start(
+      createQueueGetHandler :: createQueuePostHandler ::
+              Nil, port)
   }
 }
