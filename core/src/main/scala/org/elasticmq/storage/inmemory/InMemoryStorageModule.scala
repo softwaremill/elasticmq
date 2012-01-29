8689bb81e56a63cfbaa1787e579c48a2fea045dd
Adam Warski <adam@warski.org>
2012-01-29 12:38:52 +0100

    Nicer style

--- core/src/main/scala/org/elasticmq/storage/inmemory/InMemoryStorageModule.scala
+++ core/src/main/scala/org/elasticmq/storage/inmemory/InMemoryStorageModule.scala
@@ -2,9 +2,9 @@ package org.elasticmq.storage.inmemory
 
 import org.elasticmq.storage.StorageModule
 
-trait InMemoryStorageModule extends StorageModule with
-  InMemoryMessageStatisticsStorageModule with
-  InMemoryMessageStorageModule with
-  InMemoryMessageStorageRegistryModule with
-  InMemoryQueueStorageModule with
-  InMemoryStorageModelModule
+trait InMemoryStorageModule extends StorageModule
+  with InMemoryMessageStatisticsStorageModule
+  with InMemoryMessageStorageModule
+  with InMemoryMessageStorageRegistryModule
+  with InMemoryQueueStorageModule
+  with InMemoryStorageModelModule
--- core/src/main/scala/org/elasticmq/storage/squeryl/SquerylStorageModule.scala
+++ core/src/main/scala/org/elasticmq/storage/squeryl/SquerylStorageModule.scala
@@ -2,9 +2,9 @@ package org.elasticmq.storage.squeryl
 
 import org.elasticmq.storage.StorageModule
 
-trait SquerylStorageModule extends StorageModule with
-  SquerylInitializerModule with
-  SquerylMessageStatisticsStorageModule with
-  SquerylMessageStorageModule with
-  SquerylQueueStorageModule with
-  SquerylSchemaModule
+trait SquerylStorageModule extends StorageModule
+  with SquerylInitializerModule
+  with SquerylMessageStatisticsStorageModule
+  with SquerylMessageStorageModule
+  with SquerylQueueStorageModule
+  with SquerylSchemaModule
