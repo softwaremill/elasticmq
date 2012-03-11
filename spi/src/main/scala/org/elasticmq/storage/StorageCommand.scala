df2c24aef0bd354ebf64e7bec20fe58756c5ff38
Adam Warski <adam@warski.org>
2012-03-11 14:11:32 +0100

    Adding an idemptotent command trait

--- spi/src/main/scala/org/elasticmq/storage/StorageCommand.scala
+++ spi/src/main/scala/org/elasticmq/storage/StorageCommand.scala
@@ -7,31 +7,36 @@ sealed trait StorageCommand[R]
 
 trait MutativeCommand[R] extends StorageCommand[R]
 
-case class CreateQueueCommand(queue: QueueData) extends StorageCommand[Unit] with MutativeCommand[Unit]
-case class UpdateQueueCommand(queue: QueueData) extends StorageCommand[Unit] with MutativeCommand[Unit]
-case class DeleteQueueCommand(name: String) extends StorageCommand[Unit] with MutativeCommand[Unit]
-case class LookupQueueCommand(name: String) extends StorageCommand[Option[QueueData]]
+trait IdempotentCommand[R] extends StorageCommand[R] {
+  // Idempotent commands must be mutative
+  this: MutativeCommand[R] =>
+}
+
+case class CreateQueueCommand(queue: QueueData) extends StorageCommand[Unit] with MutativeCommand[Unit] with IdempotentCommand[Unit]
+case class UpdateQueueCommand(queue: QueueData) extends StorageCommand[Unit] with MutativeCommand[Unit] with IdempotentCommand[Unit]
+case class DeleteQueueCommand(name: String) extends StorageCommand[Unit] with MutativeCommand[Unit] with IdempotentCommand[Unit]
+case class LookupQueueCommand(name: String) extends StorageCommand[Option[QueueData]] 
 case class ListQueuesCommand() extends StorageCommand[Seq[QueueData]]
 case class GetQueueStatisticsCommand(name: String, deliveryTime: Long) extends StorageCommand[QueueStatistics]
 
 
 case class SendMessageCommand(queueName: String, message: MessageData)
-  extends StorageCommand[Unit] with MutativeCommand[Unit]
+  extends StorageCommand[Unit] with MutativeCommand[Unit] with IdempotentCommand[Unit]
 
 case class UpdateVisibilityTimeoutCommand(queueName: String, messageId: MessageId, newNextDelivery: MillisNextDelivery)
-  extends StorageCommand[Unit] with MutativeCommand[Unit]
+  extends StorageCommand[Unit] with MutativeCommand[Unit] with IdempotentCommand[Unit]
 
 case class ReceiveMessageCommand(queueName: String, deliveryTime: Long, newNextDelivery: MillisNextDelivery)
   extends StorageCommand[Option[MessageData]] with MutativeCommand[Option[MessageData]]
 
 case class DeleteMessageCommand(queueName: String, messageId: MessageId)
-  extends StorageCommand[Unit] with MutativeCommand[Unit]
+  extends StorageCommand[Unit] with MutativeCommand[Unit] with IdempotentCommand[Unit]
 
 case class LookupMessageCommand(queueName: String, messageId: MessageId) extends StorageCommand[Option[MessageData]]
 
 
 case class UpdateMessageStatisticsCommand(queueName: String, messageId: MessageId, messageStatistics: MessageStatistics)
-  extends StorageCommand[Unit] with MutativeCommand[Unit]
+  extends StorageCommand[Unit] with MutativeCommand[Unit] with IdempotentCommand[Unit]
 
 case class GetMessageStatisticsCommand(queueName: String, messageId: MessageId)
   extends StorageCommand[MessageStatistics]
