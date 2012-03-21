77be7f5899a07c5d138a6ddfcc5acf0d27020e88
Adam Warski <adam@warski.org>
2012-03-20 21:43:39 -0700

    New TC reporter

--- project/project/Plugins.scala
+++ project/project/Plugins.scala
@@ -4,6 +4,6 @@ object Plugins extends Build {
   // TeamCity reporting, see: https://github.com/guardian/sbt-teamcity-test-reporting-plugin
   lazy val plugins = Project("plugins", file("."))
     .dependsOn(
-    uri("git://github.com/guardian/sbt-teamcity-test-reporting-plugin.git#1.1"))
+    uri("git://github.com/guardian/sbt-teamcity-test-reporting-plugin.git#1.2"))
 }
 
