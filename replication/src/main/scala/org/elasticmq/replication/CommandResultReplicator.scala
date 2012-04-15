264f6e3ac66dbafb40ca0d597f0f7a2e0aedbca4
Adam Warski <adam@warski.org>
2012-04-15 11:06:31 +0200

    Moving getting mutative commands into the commands themselves

--- replication/src/main/scala/org/elasticmq/replication/CommandResultReplicator.scala
+++ replication/src/main/scala/org/elasticmq/replication/CommandResultReplicator.scala
@@ -7,37 +7,15 @@ class CommandResultReplicator(delegateStorage: StorageCommandExecutor,
                               replicationMessageSender: ReplicationMessageSender) {
 
   def replicateIfMutated[R](command: StorageCommand[R], result: R) {
-    val mutations = resultingMutations(command, result)
-    if (mutations.size > 0) {
-      replicate(mutations)
-    }
-  }
-
-  private def resultingMutations[R](command: StorageCommand[R], result: R): List[IdempotentMutativeCommand[_]] = {
     /*
      We can only replicate idempotent mutative commands. That is because during state transfer, it is possible that
      a mutation was applied to the state, which is being transferred, but the command has not yet been replicated
      (in another thread).
      So a command may end being re-applied on the new node.
     */
-    command match {
-      case c: CreateQueueCommand => c :: Nil
-      case c: UpdateQueueCommand => c :: Nil
-      case c: DeleteQueueCommand => c :: Nil
-
-      case c: SendMessageCommand => c :: Nil
-      case c: UpdateNextDeliveryCommand => c :: Nil
-      case c: DeleteMessageCommand => c :: Nil
-      case c: UpdateMessageStatisticsCommand => c :: Nil
-
-      case ReceiveMessageCommand(queueName, deliveryTime, newNextDelivery) => {
-        result match {
-          case Some(messageData) => UpdateNextDeliveryCommand(queueName, messageData.id, messageData.nextDelivery) :: Nil
-          case None => Nil
-        }
-      }
-
-      case _ => Nil
+    val mutations = command.resultingMutations(result)
+    if (mutations.size > 0) {
+      replicate(mutations)
     }
   }
 
--- spi/src/main/scala/org/elasticmq/storage/StorageCommand.scala
+++ spi/src/main/scala/org/elasticmq/storage/StorageCommand.scala
@@ -2,19 +2,25 @@ package org.elasticmq.storage
 
 import org.elasticmq.data.{MessageData, QueueData}
 import org.elasticmq.{MessageStatistics, MillisNextDelivery, MessageId, QueueStatistics}
-import java.io.{InputStream, OutputStream}
 
-sealed trait StorageCommand[R]
+sealed trait StorageCommand[R] {
+  def resultingMutations(result: R): List[IdempotentMutativeCommand[_]]
+}
 
 trait MutativeCommand[R] extends StorageCommand[R]
-trait IdempotentMutativeCommand[R] extends MutativeCommand[R]
+trait IdempotentMutativeCommand[R] extends MutativeCommand[R] {
+  def resultingMutations(result: R) = this :: Nil
+}
+trait NonMutativeCommand[R] extends StorageCommand[R] {
+  def resultingMutations(result: R) = Nil
+}
 
 case class CreateQueueCommand(queue: QueueData) extends StorageCommand[Unit] with IdempotentMutativeCommand[Unit]
 case class UpdateQueueCommand(queue: QueueData) extends StorageCommand[Unit] with IdempotentMutativeCommand[Unit]
 case class DeleteQueueCommand(name: String) extends StorageCommand[Unit] with IdempotentMutativeCommand[Unit]
-case class LookupQueueCommand(name: String) extends StorageCommand[Option[QueueData]] 
-case class ListQueuesCommand() extends StorageCommand[Seq[QueueData]]
-case class GetQueueStatisticsCommand(name: String, deliveryTime: Long) extends StorageCommand[QueueStatistics]
+case class LookupQueueCommand(name: String) extends StorageCommand[Option[QueueData]] with NonMutativeCommand[Option[QueueData]]
+case class ListQueuesCommand() extends StorageCommand[Seq[QueueData]] with NonMutativeCommand[Seq[QueueData]]
+case class GetQueueStatisticsCommand(name: String, deliveryTime: Long) extends StorageCommand[QueueStatistics] with NonMutativeCommand[QueueStatistics]
 
 
 case class SendMessageCommand(queueName: String, message: MessageData)
@@ -24,18 +30,26 @@ case class UpdateNextDeliveryCommand(queueName: String, messageId: MessageId, ne
   extends StorageCommand[Unit] with IdempotentMutativeCommand[Unit]
 
 case class ReceiveMessageCommand(queueName: String, deliveryTime: Long, newNextDelivery: MillisNextDelivery)
-  extends StorageCommand[Option[MessageData]] with MutativeCommand[Option[MessageData]]
+  extends StorageCommand[Option[MessageData]] with MutativeCommand[Option[MessageData]] {
+
+  def resultingMutations(result: Option[MessageData]) = {
+    result match {
+      case Some(messageData) => UpdateNextDeliveryCommand(queueName, messageData.id, messageData.nextDelivery) :: Nil
+      case None => Nil
+    }
+  }
+}
 
 case class DeleteMessageCommand(queueName: String, messageId: MessageId)
   extends StorageCommand[Unit] with IdempotentMutativeCommand[Unit]
 
-case class LookupMessageCommand(queueName: String, messageId: MessageId) extends StorageCommand[Option[MessageData]]
+case class LookupMessageCommand(queueName: String, messageId: MessageId) extends StorageCommand[Option[MessageData]] with NonMutativeCommand[Option[MessageData]]
 
 
 case class UpdateMessageStatisticsCommand(queueName: String, messageId: MessageId, messageStatistics: MessageStatistics)
   extends StorageCommand[Unit] with IdempotentMutativeCommand[Unit]
 
 case class GetMessageStatisticsCommand(queueName: String, messageId: MessageId)
-  extends StorageCommand[MessageStatistics]
+  extends StorageCommand[MessageStatistics] with NonMutativeCommand[MessageStatistics]
 
 case class ClearStorageCommand() extends StorageCommand[Unit] with IdempotentMutativeCommand[Unit]
