7f9c3e139285a921e4a7e6208d2754ed4db12b1e
Adam Warski <adam@warski.org>
2012-04-17 22:05:49 +0200

    Factoring out a class to create a stream of commands that dump the current state

--- replication/src/main/scala/org/elasticmq/replication/state/StateDumper.scala
+++ replication/src/main/scala/org/elasticmq/replication/state/StateDumper.scala
@@ -1,39 +1,15 @@
 package org.elasticmq.replication.state
 
-import org.elasticmq.{MessageStatistics, MessageId}
-import org.elasticmq.data.{DataSource, MessageData, QueueData}
-import org.elasticmq.storage.{StorageCommandExecutor, UpdateMessageStatisticsCommand, SendMessageCommand, CreateQueueCommand}
+import org.elasticmq.storage.StorageCommandExecutor
 import java.io.{OutputStream, ObjectOutputStream}
+import org.elasticmq.data.StateDump
 
 class StateDumper(storageCommandExecutor: StorageCommandExecutor) {
   def dump(outputStream: OutputStream) {
     val oos = new ObjectOutputStream(outputStream)
     storageCommandExecutor.executeStateManagement(dataSource => {
-      new DataSourceDumper(dataSource, oos).dumpQueues()
+      new StateDump(dataSource).createStream().foreach(oos.writeObject(_))
+      oos.writeObject(EndOfCommands)
     })
-  }
-}
-
-private class DataSourceDumper(dataSource: DataSource, oos: ObjectOutputStream) {
-  def dumpQueues() {
-    dataSource.queuesData.foreach(dumpQueue(_))
-    oos.writeObject(EndOfCommands)
-  }
-
-  def dumpQueue(queue: QueueData) {
-    oos.writeObject(CreateQueueCommand(queue))
-    dataSource.messagesData(queue).foreach(dumpMessage(queue, _))
-    dataSource.messageStatisticsWithId(queue).foreach(msWithId => dumpMessageStatistics(queue, msWithId._1, msWithId._2))
-  }
-
-  def dumpMessage(queue: QueueData, message: MessageData) {
-    oos.writeObject(SendMessageCommand(queue.name, message))
-  }
-
-  def dumpMessageStatistics(queue: QueueData, messageId: MessageId, messageStatistics: MessageStatistics) {
-    // If the receive count is 0, the same object will be created when executing the send command
-    if (messageStatistics.approximateReceiveCount != 0) {
-      oos.writeObject(UpdateMessageStatisticsCommand(queue.name, messageId, messageStatistics))
-    }
   }
 }
