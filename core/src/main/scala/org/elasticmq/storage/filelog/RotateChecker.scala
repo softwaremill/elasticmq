674d6b1a954914b8e02f6812dfef1a5219f1bdd4
Adam Warski <adam@warski.org>
2012-04-18 21:14:22 +0200

    Class for maintaining info about how much data is already written

---
+++ core/src/main/scala/org/elasticmq/storage/filelog/RotateChecker.scala
@@ -0,0 +1,22 @@
+package org.elasticmq.storage.filelog
+
+import javax.annotation.concurrent.NotThreadSafe
+
+@NotThreadSafe
+private[filelog] class RotateChecker(configuration: FileLogConfiguration) {
+  private var commandsWrittenSinceLastRotate = 0
+
+  def shouldRotate(newCommandsWritten: Int) = {
+    commandsWrittenSinceLastRotate += newCommandsWritten
+
+    if (commandsWrittenSinceLastRotate > configuration.rotateLogsAfterCommandWritten) {
+      true
+    } else {
+      false
+    }
+  }
+
+  def reset() {
+    commandsWrittenSinceLastRotate = 0
+  }
+}
