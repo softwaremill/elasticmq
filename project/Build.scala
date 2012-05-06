d4d79f2b0c5e927774b0e3ad2cacc4944462fe36
Adam Warski <adam@warski.org>
2012-05-06 12:33:05 +0200

    [WIP] creating a server distribution

--- project/Build.scala
+++ project/Build.scala
@@ -145,6 +145,112 @@ object ElasticMQBuild extends Build {
   lazy val server: Project = Project(
     "server",
     file("server"),
-    settings = buildSettings
-  ) dependsOn(core, storageDatabase, replication, restSqs)
+    settings = buildSettings ++ CustomTasks.distributionSettings
+  ) dependsOn(core, storageDatabase, replication, restSqs, commonTest % "test")
+}
+
+object CustomTasks {
+  implicit def str2pimped(s: String) = new {
+    def bold = scala.Console.BOLD + s + scala.Console.RESET
+    def green = scala.Console.GREEN + s + scala.Console.RESET
+  }
+
+  // Main settings & tasks
+  val distributionName = SettingKey[String]("distribution-name", "Name of the distribution directory")
+  val distributionDirectory = SettingKey[File]("distribution-directory", "The distribution directory")
+  val distributionLibDirectory = SettingKey[File]("distribution-lib-directory", "The distribution library directory")
+  val distributionBinDirectory = SettingKey[File]("distribution-bin-directory", "The distribution binary directory")
+  val distributionConfDirectory = SettingKey[File]("distribution-conf-directory", "The distribution configuration directory")
+
+  val distributionClean = TaskKey[Unit]("distribution-clean", "Remove previous distribution.")
+  val distribution = TaskKey[Unit]("distribution", "Create a distribution containing startup script and all jars.")
+
+  // Helper tasks
+  val distributionCopyExternalDependencies = TaskKey[Set[File]]("distribution-copy-external-dependencies", "Copies the external dependencies to the distribution directory")
+  val distributionCopyInternalDependencies = TaskKey[Set[File]]("distribution-copy-internal-dependencies", "Copies the internal dependencies to the distribution directory")
+  val distributionCopyBin = TaskKey[Set[File]]("distribution-copy-bin", "Copy binaries (scripts).")
+  val distributionCopyConf = TaskKey[Set[File]]("distribution-copy-conf", "Copy configuration.")
+  val distributionCopyDocs = TaskKey[Set[File]]("distribution-copy-docs", "Copy documentation.")
+
+  val projectDependenciesClosure = TaskKey[Set[ProjectRef]]("project-dependency-closure", "Calculates the closure of the project's dependencies, including transitive ones.")
+
+  val distributionSettings = Seq(
+    distributionName <<= (name, version) { (n, v) => n + "-" + v },
+
+    distributionDirectory <<= (target, distributionName) { (t, n) => t / "distribution" / n },
+
+    distributionLibDirectory <<= (distributionDirectory) { (dd) => dd / "lib" },
+
+    distributionBinDirectory <<= (distributionDirectory) { (dd) => dd / "bin" },
+
+    distributionConfDirectory <<= (distributionDirectory) { (dd) => dd / "conf" },
+
+    distributionCopyExternalDependencies <<= (distributionLibDirectory, externalDependencyClasspath in Compile) map { (dld, edc) =>
+      IO.copy(edc.files.map(f => (f, dld / f.getName)))
+    },
+
+    projectDependenciesClosure <<= (thisProjectRef, state) map { (thr, s) =>
+      val structure = Project.structure(s)
+
+      def isCompileConfiguration(configuration: Option[String]): Boolean = {
+        configuration.map(_.contains("compile->compile")).getOrElse(true)
+      }
+
+      def projectWithTransitiveDependencies(root: ProjectRef, acc: Set[ProjectRef]): Set[ProjectRef] = {
+        val dependencies = Project.getProject(root, structure).toList.flatMap(_.dependencies
+          // We only want compile dependencies
+          .filter(cpDep => isCompileConfiguration(cpDep.configuration))
+          .map(_.project))
+
+        dependencies.foldLeft(acc)((newAcc, dep) => {
+          if (newAcc.contains(dep)) newAcc else projectWithTransitiveDependencies(dep, newAcc + dep)
+        })
+      }
+
+      projectWithTransitiveDependencies(thr, Set(thr))
+    },
+
+    distributionCopyInternalDependencies <<= (thisProjectRef, state, distributionLibDirectory, projectDependenciesClosure) map { (thr, s, dld, pdc) =>
+      val structure = Project.structure(s)
+
+      val packageTaskKey = (packageBin in Compile).task
+
+      println("1 " + structure.data)
+      println("2 " + pdc.flatMap(p => (packageTaskKey in p).get(structure.data)))
+
+      val packageAllTask = pdc.flatMap(pr => (packageTaskKey in pr).get(structure.data)).toList
+
+      pdc.map(pr => {
+        val packageTaskOption = (packageTaskKey in pr).get(structure.data)
+        packageTaskOption.map(packageTask => packageTask.map(file => println("result: " + file)))
+      })
+
+      packageAllTask.map { jars =>
+        println("jars = " + jars)
+        jars.map(f => println("f = " + f))
+      }
+
+      Set()
+    },
+
+    distributionCopyBin <<= (distributionBinDirectory, resourceDirectory in Compile) map { (dbd, rd) =>
+      IO.copy(rd.listFiles().filter(_.getName.startsWith("run")).map(script => (script, dbd / script.getName)))
+    },
+
+    distributionCopyConf <<= (distributionConfDirectory) map { (dcd) =>
+      Set()
+    },
+
+    distributionCopyDocs <<= (distributionDirectory) map { (dd) =>
+      Set()
+    },
+
+    distributionClean <<= (distributionDirectory) map { (dd) => IO.delete(dd) },
+
+    distribution <<= (streams, version, distributionDirectory,
+      distributionCopyBin, distributionCopyConf, distributionCopyDocs,
+      distributionCopyExternalDependencies, distributionCopyInternalDependencies) map { (s, v, dd, _, _, _, _, _) =>
+      s.log.info("ElasticMQ distribution for version " + v.green + " created successfully in: " + dd.getPath)
+    }
+  )
 }
---
+++ server/src/main/resources/run.sh
@@ -0,0 +1,98 @@
+#!/bin/sh
+
+# Based on http://mojo.codehaus.org/appassembler/appassembler-maven-plugin/ (Apache2 license)
+
+# resolve links - $0 may be a softlink
+PRG="$0"
+
+while [ -h "$PRG" ]; do
+ ls=`ls -ld "$PRG"`
+ link=`expr "$ls" : '.*-> \(.*\)$'`
+ if expr "$link" : '/.*' > /dev/null; then
+   PRG="$link"
+ else
+   PRG=`dirname "$PRG"`/"$link"
+ fi
+done
+
+PRGDIR=`dirname "$PRG"`
+BASEDIR=`cd "$PRGDIR/.." >/dev/null; pwd`
+
+@ENV_SETUP@
+
+# OS specific support.  $var _must_ be set to either true or false.
+cygwin=false;
+darwin=false;
+case "`uname`" in
+ CYGWIN*) cygwin=true ;;
+ Darwin*) darwin=true
+          if [ -z "$JAVA_VERSION" ] ; then
+            JAVA_VERSION="CurrentJDK"
+          else
+            echo "Using Java version: $JAVA_VERSION"
+          fi
+          if [ -z "$JAVA_HOME" ] ; then
+            JAVA_HOME=/System/Library/Frameworks/JavaVM.framework/Versions/${JAVA_VERSION}/Home
+          fi
+          ;;
+esac
+
+if [ -z "$JAVA_HOME" ] ; then
+ if [ -r /etc/gentoo-release ] ; then
+   JAVA_HOME=`java-config --jre-home`
+ fi
+fi
+
+# For Cygwin, ensure paths are in UNIX format before anything is touched
+if $cygwin ; then
+ [ -n "$JAVA_HOME" ] && JAVA_HOME=`cygpath --unix "$JAVA_HOME"`
+ [ -n "$CLASSPATH" ] && CLASSPATH=`cygpath --path --unix "$CLASSPATH"`
+fi
+
+# If a specific java binary isn't specified search for the standard 'java' binary
+if [ -z "$JAVACMD" ] ; then
+ if [ -n "$JAVA_HOME"  ] ; then
+   if [ -x "$JAVA_HOME/jre/sh/java" ] ; then
+     # IBM's JDK on AIX uses strange locations for the executables
+     JAVACMD="$JAVA_HOME/jre/sh/java"
+   else
+     JAVACMD="$JAVA_HOME/bin/java"
+   fi
+ else
+   JAVACMD=`which java`
+ fi
+fi
+
+if [ ! -x "$JAVACMD" ] ; then
+ echo "Error: JAVA_HOME is not defined correctly." 1>&2
+ echo "  We cannot execute $JAVACMD" 1>&2
+ exit 1
+fi
+
+if [ -z "$REPO" ]
+then
+ REPO="$BASEDIR"/@REPO@
+fi
+
+CLASSPATH=$CLASSPATH_PREFIX:@CLASSPATH@
+EXTRA_JVM_ARGUMENTS="@EXTRA_JVM_ARGUMENTS@"
+
+# For Cygwin, switch paths to Windows format before running java
+if $cygwin; then
+ [ -n "$CLASSPATH" ] && CLASSPATH=`cygpath --path --windows "$CLASSPATH"`
+ [ -n "$JAVA_HOME" ] && JAVA_HOME=`cygpath --path --windows "$JAVA_HOME"`
+ [ -n "$HOME" ] && HOME=`cygpath --path --windows "$HOME"`
+ [ -n "$BASEDIR" ] && BASEDIR=`cygpath --path --windows "$BASEDIR"`
+ [ -n "$REPO" ] && REPO=`cygpath --path --windows "$REPO"`
+fi
+
+exec "$JAVACMD" $JAVA_OPTS \
+ $EXTRA_JVM_ARGUMENTS \
+ -classpath "$CLASSPATH" \
+ -Dapp.name="@APP_NAME@" \
+ -Dapp.pid="$$" \
+ -Dapp.repo="$REPO" \
+ -Dapp.home="$BASEDIR" \
+ -Dbasedir="$BASEDIR" \
+ @MAINCLASS@ \
+ @APP_ARGUMENTS@"$@"
