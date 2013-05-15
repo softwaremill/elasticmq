2c3e27c24bc5b1ffcf8a3e2e6a7954ae14e8360f
adamw <adam@warski.org>
2013-05-15 14:29:29 +0200

    Making extra sure that the tests aren't parallel

--- project/Build.scala
+++ project/Build.scala
@@ -45,6 +45,8 @@ object BuildSettings {
       </developers>),
 
     parallelExecution := false,
+    // workaround for: https://github.com/sbt/sbt/issues/692
+    fork in Test := true,
     scalacOptions += "-unchecked",
     homepage      := Some(new java.net.URL("http://www.elasticmq.org")),
     licenses      := ("Apache2", new java.net.URL("http://www.apache.org/licenses/LICENSE-2.0.txt")) :: Nil
