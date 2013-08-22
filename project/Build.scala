14ce05bbcab37be408b907c5b1a07ff1b38c4611
adamw <adam@warski.org>
2013-08-22 14:57:55 +0200

    0.7.2 development

--- project/Build.scala
+++ project/Build.scala
@@ -7,7 +7,7 @@ import ls.Plugin._
 object BuildSettings {
   val buildSettings = Defaults.defaultSettings ++ Seq (
     organization  := "org.elasticmq",
-    version       := "0.7.1",
+    version       := "0.7.2-SNAPSHOT",
     scalaVersion  := "2.10.2",
 
     resolvers += "spray repo" at "http://repo.spray.io", // TODO
