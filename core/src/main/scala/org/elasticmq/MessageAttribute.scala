d01f68995536baa028bfb9c78adaaaa6939c1fcc
Alexander Eliseyev <a.a.eliseyev@gmail.com>
2015-08-10 16:09:07 +0300

    Binary message attribute unit test

--- core/src/main/scala/org/elasticmq/MessageAttribute.scala
+++ core/src/main/scala/org/elasticmq/MessageAttribute.scala
@@ -1,5 +1,6 @@
 package org.elasticmq
 
+import java.nio.ByteBuffer
 import javax.xml.bind.DatatypeConverter
 
 sealed abstract class MessageAttribute(val customType: Option[String]) {
@@ -11,18 +12,28 @@ sealed abstract class MessageAttribute(val customType: Option[String]) {
   }
 }
 
-case class StringMessageAttribute(stringValue: String, override val customType: Option[String]) extends MessageAttribute(customType) {
+case class StringMessageAttribute(stringValue: String, override val customType: Option[String] = None) extends MessageAttribute(customType) {
   protected override val primaryDataType: String = "String"
 }
 
-case class BinaryMessageAttribute(binaryValue: Array[Byte], override val customType: Option[String]) extends MessageAttribute(customType) {
+case class BinaryMessageAttribute(binaryValue: Array[Byte], override val customType: Option[String] = None) extends MessageAttribute(customType) {
   protected override val primaryDataType: String = "Binary"
 
   def asBase64 = DatatypeConverter.printBase64Binary(binaryValue)
 }
 object BinaryMessageAttribute {
-  def fromBase64(base64Str: String, customType: Option[String]) = BinaryMessageAttribute(
+  def fromBase64(base64Str: String, customType: Option[String] = None) = BinaryMessageAttribute(
     binaryValue = DatatypeConverter.parseBase64Binary(base64Str),
+    customType = customType
+  )
+
+  def fromByteBuffer(byteBuffer: ByteBuffer, customType: Option[String] = None) = BinaryMessageAttribute(
+    binaryValue = {
+      byteBuffer.clear()
+      val value = new Array[Byte](byteBuffer.capacity())
+      byteBuffer.get(value)
+      value
+    },
     customType = customType
   )
 }
