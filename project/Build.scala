d08b38235354453a3793b4f998ffa1e743b1469d
adamw <adam@warski.org>
2016-04-13 15:36:24 +0200

    0.9.3 release

--- project/Build.scala
+++ project/Build.scala
@@ -6,7 +6,7 @@ import AssemblyKeys._
 object BuildSettings {
   val buildSettings = Defaults.coreDefaultSettings ++ Seq (
     organization  := "org.elasticmq",
-    version       := "0.9.2",
+    version       := "0.9.3",
     scalaVersion  := "2.11.8",
 
     libraryDependencies += "org.scala-lang.modules" %% "scala-xml" % "1.0.5",
