76c08fd8f34abc1701d859ea394f8286e8747dad
Adam Fish Rybicki <adamrybicki@adams-macbook-pro.home>
2023-05-22 08:02:36 +0200

    Purgequeue impl

--- rest/rest-sqs/src/main/scala/org/elasticmq/rest/sqs/PurgeQueueDirectives.scala
+++ rest/rest-sqs/src/main/scala/org/elasticmq/rest/sqs/PurgeQueueDirectives.scala
@@ -6,11 +6,16 @@ import org.elasticmq.msg.ClearQueue
 import org.elasticmq.rest.sqs.Action.PurgeQueue
 import org.elasticmq.rest.sqs.Constants._
 import org.elasticmq.rest.sqs.directives.ElasticMQDirectives
+import org.elasticmq.rest.sqs.model.RequestPayload
+import spray.json.DefaultJsonProtocol.{StringJsonFormat, jsonFormat1}
+import spray.json.RootJsonFormat
 
 trait PurgeQueueDirectives { this: ElasticMQDirectives with QueueURLModule =>
-  def purgeQueue(p: AnyParams, protocol: AWSProtocol) = {
+  def purgeQueue(p: RequestPayload, protocol: AWSProtocol) = {
     p.action(PurgeQueue) {
-      queueActorFromRequest(p) { queueActor =>
+      val requestParams = p.as[PurgeQueueActionRequest]
+
+      queueActorFromUrl(requestParams.QueueUrl){ queueActor =>
         for {
           _ <- queueActor ? ClearQueue()
         } yield {
@@ -28,6 +33,19 @@ trait PurgeQueueDirectives { this: ElasticMQDirectives with QueueURLModule =>
             case _ => complete(status = 200, HttpEntity.Empty)
           }
         }
+      }
+    }
+  }
+
+  case class PurgeQueueActionRequest(QueueUrl: String)
+
+  object PurgeQueueActionRequest {
+    implicit val requestJsonFormat: RootJsonFormat[PurgeQueueActionRequest] = jsonFormat1(PurgeQueueActionRequest.apply)
+
+    implicit val requestParamReader: FlatParamsReader[PurgeQueueActionRequest] = new FlatParamsReader[PurgeQueueActionRequest] {
+      override def read(params: Map[String, String]): PurgeQueueActionRequest = {
+        val queueUrl = requiredParameter(params)("QueueUrl")
+        PurgeQueueActionRequest(queueUrl)
       }
     }
   }
