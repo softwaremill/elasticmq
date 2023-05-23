ed56deda6d3a3d2c820e1cefa66f28974627a9ef
Adam Fish Rybicki <adamrybicki@adams-macbook-pro.home>
2023-05-23 09:26:28 +0200

    add permissions

--- rest/rest-sqs/src/main/scala/org/elasticmq/rest/sqs/AddPermissionDirectives.scala
+++ rest/rest-sqs/src/main/scala/org/elasticmq/rest/sqs/AddPermissionDirectives.scala
@@ -1,18 +1,27 @@
 package org.elasticmq.rest.sqs
 
 import Constants._
+import akka.http.scaladsl.model.HttpEntity
 import org.elasticmq.rest.sqs.Action.AddPermission
 import org.elasticmq.rest.sqs.directives.ElasticMQDirectives
+import spray.json.RootJsonFormat
+import spray.json.DefaultJsonProtocol._
+import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
+import org.elasticmq.rest.sqs.model.RequestPayload
 
 trait AddPermissionDirectives { this: ElasticMQDirectives with QueueURLModule =>
-  def addPermission(p: AnyParams) = {
+  def addPermission(p: RequestPayload, protocol: AWSProtocol) = {
     p.action(AddPermission) {
-      respondWith {
-        <AddPermissionResponse>
-          <ResponseMetadata>
-            <RequestId>{EmptyRequestId}</RequestId>
-          </ResponseMetadata>
-        </AddPermissionResponse>
+      protocol match {
+        case AWSProtocol.AWSQueryProtocol =>
+          respondWith {
+            <AddPermissionResponse>
+              <ResponseMetadata>
+                <RequestId>{EmptyRequestId}</RequestId>
+              </ResponseMetadata>
+            </AddPermissionResponse>
+          }
+        case _ => complete(200, HttpEntity.Empty)
       }
     }
   }
