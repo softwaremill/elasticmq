56f34be619ca544fa7e98252ec75e5c55b13c1d8
Adam Fish Rybicki <adamrybicki@adams-macbook-pro.home>
2023-05-30 08:17:59 +0200

    types + fmt

--- rest/rest-sqs/src/main/scala/org/elasticmq/rest/sqs/AkkaSupport.scala
+++ rest/rest-sqs/src/main/scala/org/elasticmq/rest/sqs/AkkaSupport.scala
@@ -1,29 +1,34 @@
 package org.elasticmq.rest.sqs
 
-import akka.http.javadsl.server.RouteResult
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
 import spray.json.RootJsonFormat
 import akka.http.scaladsl.marshalling.Marshaller
 import akka.http.scaladsl.model.ContentTypes._
 import akka.http.scaladsl.model.{HttpEntity, RequestEntity}
+import akka.http.scaladsl.server
 import akka.http.scaladsl.server.Directives.complete
-import akka.http.scaladsl.server.RequestContext
 import akka.util.ByteString
 import org.elasticmq.rest.sqs.Constants.EmptyRequestId
 import org.elasticmq.rest.sqs.directives.RespondDirectives
 
-import scala.concurrent.Future
 import scala.xml._
 import scala.xml.{Null, UnprefixedAttribute}
 
 trait AkkaSupport {
   _: RespondDirectives =>
 
-  private def namespace[T](body: UnprefixedAttribute => Marshaller[T, RequestEntity])(implicit v: XmlNsVersion): Marshaller[T, RequestEntity] = {
+  private def namespace[T](
+      body: UnprefixedAttribute => Marshaller[T, RequestEntity]
+  )(implicit v: XmlNsVersion): Marshaller[T, RequestEntity] = {
     body(new UnprefixedAttribute("xmlns", "http://queue.amazonaws.com/doc/%s/".format(v.version), Null))
   }
 
-  implicit def elasticMQMarshaller[T](implicit xmlSerializer: XmlSerializer[T], json: RootJsonFormat[T], protocol: AWSProtocol, version: XmlNsVersion) =
+  implicit def elasticMQMarshaller[T](implicit
+      xmlSerializer: XmlSerializer[T],
+      json: RootJsonFormat[T],
+      protocol: AWSProtocol,
+      version: XmlNsVersion
+  ): Marshaller[T, RequestEntity] =
     protocol match {
       case AWSProtocol.`AWSJsonProtocol1.0` => sprayJsonMarshaller[T]
       case _ =>
@@ -35,14 +40,16 @@ trait AkkaSupport {
         }
     }
 
-  def emptyResponse(xmlTagName: String)(implicit protocol: AWSProtocol) = {
+  def emptyResponse(xmlTagName: String)(implicit protocol: AWSProtocol): server.Route = {
     protocol match {
-      case AWSProtocol.`AWSJsonProtocol1.0` =>complete(200, HttpEntity.Empty)
+      case AWSProtocol.`AWSJsonProtocol1.0` => complete(200, HttpEntity.Empty)
       case _ =>
         respondWith {
           <wrapper>
             <ResponseMetadata>
-              <RequestId>{EmptyRequestId}</RequestId>
+              <RequestId>
+                {EmptyRequestId}
+              </RequestId>
             </ResponseMetadata>
           </wrapper> % Attribute(None, "name", Text(xmlTagName), Null)
         }
