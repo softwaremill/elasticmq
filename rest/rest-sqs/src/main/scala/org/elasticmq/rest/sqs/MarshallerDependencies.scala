22dcfc0a1622dbeaeee1b9536aea1ad9233d2f67
Adam Fish Rybicki <adamrybicki@adams-macbook-pro.home>
2023-05-31 08:41:56 +0200

    formatting

--- rest/rest-sqs/src/main/scala/org/elasticmq/rest/sqs/MarshallerDependencies.scala
+++ rest/rest-sqs/src/main/scala/org/elasticmq/rest/sqs/MarshallerDependencies.scala
@@ -4,7 +4,7 @@ import akka.http.scaladsl.server.Directives._
 import org.elasticmq.rest.sqs.Constants.SqsDefaultVersion
 import scala.language.postfixOps
 
-case class MarshallerDependencies (protocol: AWSProtocol, xmlNsVersion: XmlNsVersion)
+case class MarshallerDependencies(protocol: AWSProtocol, xmlNsVersion: XmlNsVersion)
 
 sealed trait AWSProtocol extends Product with Serializable
 
