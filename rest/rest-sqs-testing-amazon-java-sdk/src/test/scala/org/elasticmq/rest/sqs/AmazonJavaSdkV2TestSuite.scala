ca8eb380cb4c43b3ccdbf7e5ef6ebca3fea30018
Michał Ossowski <michal.ossowski@softwaremill.com>
2023-11-15 10:40:13 +0100

    fix 2.12 build

--- rest/rest-sqs-testing-amazon-java-sdk/src/test/scala/org/elasticmq/rest/sqs/AmazonJavaSdkV2TestSuite.scala
+++ rest/rest-sqs-testing-amazon-java-sdk/src/test/scala/org/elasticmq/rest/sqs/AmazonJavaSdkV2TestSuite.scala
@@ -6,15 +6,15 @@ import software.amazon.awssdk.services.sqs.model._
 class AmazonJavaSdkV2TestSuite extends SqsClientServerWithSdkV2Communication with Matchers {
 
   test("should create a queue") {
-    clientV2.createQueue(CreateQueueRequest.builder.queueName("testQueue1").build())
+    clientV2.createQueue(CreateQueueRequest.builder().queueName("testQueue1").build())
   }
 
   test("should get queue url") {
     // Given
-    clientV2.createQueue(CreateQueueRequest.builder.queueName("testQueue1").build())
+    clientV2.createQueue(CreateQueueRequest.builder().queueName("testQueue1").build())
 
     // When
-    val queueUrl = clientV2.getQueueUrl(GetQueueUrlRequest.builder.queueName("testQueue1").build()).queueUrl()
+    val queueUrl = clientV2.getQueueUrl(GetQueueUrlRequest.builder().queueName("testQueue1").build()).queueUrl()
 
     // Then
     queueUrl shouldEqual "http://localhost:9321/123456789012/testQueue1"
@@ -23,7 +23,7 @@ class AmazonJavaSdkV2TestSuite extends SqsClientServerWithSdkV2Communication wit
   test("should fail to get queue url if queue doesn't exist") {
     // When
     val thrown = intercept[QueueDoesNotExistException] {
-      clientV2.getQueueUrl(GetQueueUrlRequest.builder.queueName("testQueue1").build()).queueUrl()
+      clientV2.getQueueUrl(GetQueueUrlRequest.builder().queueName("testQueue1").build()).queueUrl()
     }
 
     // Then
